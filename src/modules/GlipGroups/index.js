f08bb7e9de3a047094f3ff72ddb4cf1d97ddeeed
Embbnux <dongyu666@hotmail.com>
2018-05-27 17:48:09 +0800

    allow diffrent group id in different tabs

---
+++ src/modules/GlipGroups/index.js
@@ -0,0 +1,45 @@
+import GlipGroups from 'ringcentral-integration/modules/GlipGroups';
+import { Module } from 'ringcentral-integration/lib/di';
+
+import getReducer, {
+  getCurrentGroupIdReducer,
+} from 'ringcentral-integration/modules/GlipGroups/getReducer';
+
+@Module({
+  deps: []
+})
+export default class NewGlipGroups extends GlipGroups {
+  constructor(options) {
+    super(options);
+
+    if (this._storage) {
+      this._reducer = getReducer(this.actionTypes, {
+        currentGroupId: getCurrentGroupIdReducer(this.actionTypes)
+      });
+    }
+  }
+
+  _onDataReady() {
+    if (this._glipPersons) {
+      this._glipPersons.loadPersons(this.groupMemberIds);
+    }
+    if (!this.currentGroupId && this.currentGroupIdFromStorage) {
+      this.updateCurrentGroupId(this.currentGroupIdFromStorage);
+    }
+    if (!this.currentGroupId && !this.currentGroupIdFromStorage) {
+      this.updateCurrentGroupId(this.groups[0] && this.groups[0].id);
+    }
+    if (this._preloadPosts) {
+      this._preloadedPosts = {};
+      this._preloadGroupPosts();
+    }
+  }
+
+  get currentGroupId() {
+    return this.state.currentGroupId;
+  }
+
+  get currentGroupIdFromStorage() {
+    return this.state.currentGroupId;
+  }
+}
