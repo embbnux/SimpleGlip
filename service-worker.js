59a18cefb98f441d12da6bc0011c272d9aa2b8fa
Embbnux Ji (from Travis CI) <embbnux@embbnux.com>
2019-07-29 09:32:50 +0000

    Deploy embbnux/SimpleGlip to github.com/embbnux/SimpleGlip.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,83 @@
+var cacheStorageKey = 'simple-glip-pwa';
+var offlineRequestStorageKey = 'simple-glip-pwa-offline';
+
+var cacheList = [
+  'index.js',
+  'assets/images/favicon.png',
+  'assets/images/favicon192.jpg',
+  'assets/images/favicon512.jpg',
+  'redirect.html',
+  'redirect.js',
+  'proxy.html',
+  'proxy.js'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (name) {
+        if (name !== cacheStorageKey) {
+          return caches.delete(name);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+function fetchAndCache(request) {
+  const cloneRequest = request.clone();
+  return fetch(cloneRequest).then(function (httpRes) {
+    if (!httpRes || httpRes.status > 210) {
+      return httpRes;
+    }
+    var responseClone = httpRes.clone();
+    caches.open(offlineRequestStorageKey).then(function (cache) {
+      cache.put(request, responseClone);
+    });
+    return httpRes;
+  });
+}
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') {
+    return;
+  }
+  if (e.request.url.indexOf('pubnub.com') > -1) {
+    return;
+  }
+  if (e.request.url.indexOf('pndsn.com') > -1) {
+    return;
+  }
+  if (e.request.url.indexOf('manifest.json') > -1) {
+    return;
+  }
+  if (e.request.url.indexOf('s3.amazonaws.com') > -1) {
+    return;
+  }
+  if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') {
+    return;
+  }
+  if (self.navigator.onLine && e.request.headers.get('accept').indexOf('application/json') > -1) {
+    e.respondWith(
+      fetchAndCache(e.request)
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetchAndCache(e.request);
+    })
+  );
+});
